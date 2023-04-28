9cc628fb50033d4a594d76ac136ba0359ad8e969
Stefano Frasson Pianizzola <frasson.stefano@gmail.com>
2023-04-28 18:43:08 +0200

    Updated the countries-cities js file with the right coordinates because some countries have some cities with the same names

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '70d8a720e342208b1e00');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f8d6f17a02106fbd568e');
